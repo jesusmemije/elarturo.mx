d4237dfcf410937782ee83ece80cc243baa19abc
Jesús González <memije.dev@gmail.com>
2021-04-16 16:42:44 -0500

    Effects Imágenes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
  <!DOCTYPE html>
  <html>
+
  <head>
  	<meta charset="utf-8">
  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -12,6 +13,7 @@
  	<!-- Custom styles -->
  	<link rel="stylesheet" href="css/styles.css">
  </head>
+
  <body>
 
  	<!-- Slider main container -->
@@ -19,12 +21,85 @@
  		<!-- Additional required wrapper -->
  		<div class="swiper-wrapper">
  			<!-- Slides -->
- 			<!-- swiper-zoom-container -->
- 			<div class="swiper-slide bg-size-img" style="background-image: url('img/pasado.jpg');"></div>
- 			<div class="swiper-slide bg-size-img" style="background-image: url('img/vida-mas-easy.jpg');"></div>
- 			<div class="swiper-slide bg-size-img" style="background-image: url('img/y-que.jpg');"></div>
- 			<div class="swiper-slide bg-size-img" style="background-image: url('img/apenas-empezaba.jpg');"></div>
- 			<div class="swiper-slide bg-size-img" style="background-image: url('img/home.jpg');"></div>
+ 			<div class="swiper-slide bg-size-img" style="background-image: url('img/pasado.jpg');">
+ 				<div style="position: absolute; top: 60%; left: 50%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/pasado/personaje.png" width="150" class="img-fluid hvr-grow" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 44%; left: 40%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/pasado/guitarra.png" width="80" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 40%; left: 60%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/pasado/poster.png" width="120" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 85%; left: 40%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/pasado/amplificador.png" width="100" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 85%; left: 60%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/pasado/coyote.png" width="100" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 			</div>
+ 			<div class="swiper-slide bg-size-img" style="background-image: url('img/vida-mas-easy.jpg');">
+ 				<div style="position: absolute; top: 62%; left: 53%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/la-vida-era-mas-easy/cuerpo.png" width="250" class="img-fluid hvr-grow" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 50%; left: 36%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/la-vida-era-mas-easy/cel.png" width="80" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 46%; left: 47%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/la-vida-era-mas-easy/hojas.png" width="75" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 46%; left: 62%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/la-vida-era-mas-easy/coyote.png" width="100" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 			</div>
+ 			<div class="swiper-slide bg-size-img" style="background-image: url('img/y-que.jpg');">
+ 				<div style="position: absolute; top: 62%; left: 50%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/y-que/personaje.png" width="350" class="img-fluid hvr-grow" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 68%; left: 33%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/y-que/cuervo.png" width="160" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 48%; left: 55%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/y-que/coyote.png" width="80" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 88%; left: 62%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/y-que/lata.png" width="130" class="img-fluid hvr-rotate" alt="">
+ 				</div>
+ 			</div>
+ 			<div class="swiper-slide bg-size-img" style="background-image: url('img/apenas-empezaba.jpg');">
+ 				<div style="position: absolute; top: 75%; left: 44%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/apenas-empezaba/cuerpo.png" width="500" class="img-fluid hvr-grow" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/apenas-empezaba/cenicero.png" width="230" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 58%; left: 52%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/apenas-empezaba/bote-basura.png" width="140" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 50%; left: 62%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/apenas-empezaba/coyote.png" width="80" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 80%; left: 58%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/apenas-empezaba/libreta.png" width="190" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 			</div>
+ 			<div class="swiper-slide bg-size-img" style="background-image: url('img/presente.jpg');">
+ 				<div style="position: absolute; top: 68%; left: 50%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/presente/productor.png" width="250" class="img-fluid hvr-grow" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 22%; left: 50%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/presente/guitarra.png" width="130" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 35%; left: 40%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/presente/coyote.png" width="120" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 40%; left: 60%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/presente/corazon.png" width="100" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 				<div style="position: absolute; top: 58%; left: 35%; transform: translate(-50%, -50%); cursor: pointer;">
+ 					<img src="icons/presente/nota-musical.png" width="100" class="img-fluid hvr-buzz-out" alt="">
+ 				</div>
+ 			</div>
  		</div>
  		<!-- If we need pagination -->
  		<div class="swiper-pagination"></div>
@@ -35,21 +110,21 @@
 
  		<img src="icons/linea.png" class="line-pagination" alt="">
 
- 		<div class="swiper-icons-social"> 
+ 		<div class="swiper-icons-social">
  			<a href="https://www.facebook.com/" class="hvr-buzz-out" target="_BLANK">
- 				<img src="icons/fb-icon.png" width="42" class="animate__animated animate__bounce" alt="">
+ 				<img src="icons/redes/fb-icon.png" width="42" class="animate__animated animate__bounce" alt="">
  			</a>
  			<a href="https://www.instagram.com/" class="hvr-buzz-out" target="_BLANK">
- 				<img src="icons/insta-icon.png" width="42" class="animate__animated animate__bounce" alt="">
+ 				<img src="icons/redes/insta-icon.png" width="42" class="animate__animated animate__bounce" alt="">
  			</a>
  			<a href="https://" class="hvr-buzz-out" target="_BLANK">
- 				<img src="icons/icon.png" width="42" class="animate__animated animate__bounce" alt="">
+ 				<img src="icons/redes/icon.png" width="42" class="animate__animated animate__bounce" alt="">
  			</a>
  			<a href="https://www.youtube.com/" class="hvr-buzz-out" target="_BLANK">
- 				<img src="icons/youtube-icon.png" width="42" class="animate__animated animate__bounce" alt="">
+ 				<img src="icons/redes/youtube-icon.png" width="42" class="animate__animated animate__bounce" alt="">
  			</a>
  			<a href="https://www.tiktok.com/" class="hvr-buzz-out" target="_BLANK">
- 				<img src="icons/tiktok-icon.png" width="42" class="animate__animated animate__bounce" alt="">
+ 				<img src="icons/redes/tiktok-icon.png" width="42" class="animate__animated animate__bounce" alt="">
  			</a>
  		</div>
 
@@ -61,4 +136,5 @@
  	<!-- Main js -->
  	<script src="js/app.js"></script>
  </body>
- </html> 
+
+ </html>
